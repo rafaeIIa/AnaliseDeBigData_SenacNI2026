06206e2a04babb3c4eabda468a9f47494ab61cd1
Rafaella <r.a.almeida002@gmail.com>
2026-04-24 19:59:24 -0300

    Implement examples of data structures in Aula6.py
    Added examples of lists, tuples, sets, and dictionaries in Python.

---
+++ UC1/Aula6/Aula6.py
@@ -0,0 +1,23 @@
+## LISTAS
+
+nomes = ['Matheus','Alice','Caio','Larissa','Miguel','Rafael']
+
+# lista1=[]
+# tupla1=()
+# set1=() #ou set={} #não tem ordem
+# dict1={:} #é mais organizado. Rótulo/chave:valor ex: "Idade:19" .json
+
+#TUPLAS:
+nomes_tupla=tuple(nomes)
+print(nomes_tupla)
+#SET
+nomes_set=set(nomes)
+print(nomes_set)
+#DICIONÁRIOS
+nomes_dict={'nome1':'Matheus',
+            'nome2': 'Alice',
+            'nome3': 'Caio',
+            'nome4': 'Larissa',
+            'nome5': 'Miguel',
+            'nome6': 'Rafael'}
+print(nomes_dict)
