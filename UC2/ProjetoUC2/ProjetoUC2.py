f6764d867d872679604c16ba9425b4bd2457a3a1
Rafaella <r.a.almeida002@gmail.com>
2026-05-02 00:40:07 -0300

    Importação e limpeza - ProjetoUC2

---
+++ UC2/ProjetoUC2/ProjetoUC2.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+###### IMPORTAÇÃO E LIMPEZA ######
+
+# Importando o .csv, definindo o separador (;) e encoding
+df_pocos_exploratorios = pd.read_csv('pocos-exploratorios-jan26.csv', sep=';', encoding='latin1')
+
+# Criando a cópia do arquivo que será utilizada ao longo da limpeza e cálculos estatíscos
+df_pocos_copy = df_pocos_exploratorios.copy()
+
+# Definindo as primeiras e últimas 5 linhas para visualização
+print(df_pocos_copy.head())
+print(df_pocos_copy.tail())
+
+# Definindo o tipo de dado das colunas para verificar o que precisa ser modificado
+print(df_pocos_copy.info())
+
+# Renomeando maior parte das colunas
+df_pocos_copy.columns = ['id_poco', 'id_bloco', 'localizacao_bacia', 'regiao_bacia', 
+'estado', 'ambiente', 'operador', 'data_inicio', 'data_conclusao', 'profundidade(m)',
+'pre_sal',  'notificacao_descoberta', 'data_primeira_descoberta', 'fluido_notificacao_descoberta']
+
+#### INÍCIO DA LIMPEZA ######
+
+# Alterando o tipo de dado das colunass para data e definindo parâmetros
+df_pocos_copy['data_inicio'] = pd.to_datetime(df_pocos_copy['data_inicio'], dayfirst=True, errors='coerce')
+df_pocos_copy['data_conclusao'] = pd.to_datetime(df_pocos_copy['data_conclusao'], dayfirst=True, errors='coerce')
+df_pocos_copy['data_primeira_descoberta'] = pd.to_datetime(df_pocos_copy['data_primeira_descoberta'], dayfirst=True, errors='coerce')
+df_pocos_copy['profundidade(m)'] = pd.to_numeric(df_pocos_copy['profundidade(m)'], errors='coerce')
+
+# Verificando o total de linhas por coluna com valor NULO
+print(df_pocos_copy.isnull().sum())
+
+# Verificando o total de linhas por coluna COM ESPAÇO/VAZIO 
+espacos_vazios = (df_pocos_copy == ' ').sum()
+print(espacos_vazios)   # Resultado de espaços vazios = 0
+
+# Preenchendo espaços vazios das colunas de tipo texto
+colunas_str = ['id_poco', 'id_bloco', 'localizacao_bacia', 'regiao_bacia', 'estado', 'ambiente',
+'operador', 'pre_sal', 'notificacao_descoberta', 'fluido_notificacao_descoberta']
+
+df_pocos_copy[colunas_str] = df_pocos_copy[colunas_str].fillna('Não informado')   
+
+print(df_pocos_copy) 
+print(df_pocos_copy.describe())
