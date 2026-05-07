80f3de9717fa54a38cc892252c6c305ea8001750
Rafaella <r.a.almeida002@gmail.com>
2026-05-06 21:43:17 -0300

    Atualiza aula 11

---
+++ UC2/Aula11/aula11.py
@@ -0,0 +1,7 @@
+# Se a ativação da máquina virtual não funcionar, executar codigo antes:
+
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser 
+
+# .parquet, .feather, .pickle = tipo de arquivo que aceta .zip ----- .parquet
+
+#segunda fase, converter arquivo para parquet
